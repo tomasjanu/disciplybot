22b8bb5396dd53f26ea5ffa8957eba432dd43058
Tom Janu <tomas.janu@gmail.com>
2022-08-17 00:11:08 +0200

    Celý  jsem to opravil a teď to jede
    btw tohle je Jindra aka @jindrahelcl

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ rtc = DS1307(i2c_rtc)
 ledRed1 = Pin(20, Pin.OUT)
 ledRed2 = Pin(19, Pin.OUT)
 ledRed3 = Pin(18, Pin.OUT)
-ledPowerRed = Pin(27, Pin.OUT)
-ledPowerGreen = Pin(26, Pin.OUT)
+ledPowerRed = Pin(26, Pin.OUT)
+ledPowerGreen = Pin(27, Pin.OUT)
 
 # Buttons
 button1 = Pin(11, Pin.IN, Pin.PULL_DOWN)
@@ -19,70 +19,39 @@ button2 = Pin(12, Pin.IN, Pin.PULL_DOWN)
 button3 = Pin(13, Pin.IN, Pin.PULL_DOWN)
 buttonAddHour = Pin(14, Pin.IN, Pin.PULL_DOWN)
 
+startOfTasksHour = 6
+startOfTasksMinute = 0
+
+print("Start: " + str(rtc.datetime()))
+
 task1 = False
 task2 = False
 task3 = False
 dayFinished = False
-dayNulled = True
-testHours = 4
-startOfTasksDay = 6
-startOfNulling = 4
-
-print("Start: " + str(rtc.datetime()))
+(year,month,date,day,hour,minute,second,p1)=rtc.datetime()
+dayNulled = date - 1
+print(dayNulled)
 
 def refreshDay():
     global task1
     global task2
     global task3
     global dayFinished
-    global dayNulled
-
+    
     task1 = False
     task2 = False
     task3 = False
-
     dayFinished = False
-    dayNulled = True
 
     print("Day refreshed")
-    time.sleep(0.5)
 
-while True:
-    (year,month,date,day,hour,minute,second,p1)=rtc.datetime()
-
-    if(dayFinished == False):
-        if not task1:
-            ledRed1(0) #zapni
-        if not task2:
-            ledRed2(0) #zapni
-        if not task3:        
-            ledRed3(0) #zapni
-        
-    if button1.value() == 1:
-        ledRed1(1) # vypni
-        task1 = True
-        print("Task1 splnen")
-        time.sleep(0.5)
-        
-    if button2.value() == 1:
-        ledRed2(1) # vypni
-        task2 = True
-        print("Task2 splnen")
-        time.sleep(0.5)
-          
-    if button3.value() == 1:
-        ledRed3(1) # vypni
-        task3 = True
-        print("Task3 splnen")
-        time.sleep(0.5)
+def update_leds():
+    global dayFinished
     
-    if buttonAddHour.value() == 1:
-        if testHours == 24:
-            testHours = 1
-        testHours+=1
-        print("Hours:" + str(testHours))
-        time.sleep(0.5)
-
+    ledRed1(task1) # not protoze hardware je blbe
+    ledRed2(task2)
+    ledRed3(task3)
+    
     if task1 and task2 and task3:
         dayFinished = True
 
@@ -92,9 +61,37 @@ while True:
     else:
         ledPowerGreen(0)
         ledPowerRed(1)
+    
 
-    if hour > startOfTasksDay and not dayNulled:
+i = 0
+
+while True:
+    (year,month,date,day,hour,minute,second,p1)=rtc.datetime()
+        
+    i += 1
+    if i % 25 == 0:
+        print("current time: {}:{}:{}".format(hour,minute,second))
+        print("last day of nulling: {}".format(dayNulled))
+
+    update_leds()
+    
+    if button1.value() == 1 and not task1:
+        #ledRed1(1) # vypni
+        task1 = True
+        print("Task1 splnen")
+        
+    if button2.value() == 1 and not task2:
+        task2 = True
+        print("Task2 splnen")
+          
+    if button3.value() == 1 and not task3:
+        task3 = True
+        print("Task3 splnen")
+            
+    if dayNulled != date and (hour > startOfTasksHour or (hour == startOfTasksHour and minute >= startOfTasksMinute)):
+        print("refreshing: ", dayNulled != date, hour > startOfTasksHour, hour == startOfTasksHour and startOfTasksMinute >= minute)
         refreshDay()
-
-    if hour > startOfNulling and hour < startOfTasksDay and dayNulled:
-        dayNulled = False
+        dayNulled = date        
+        
+    time.sleep(0.05)
+        
